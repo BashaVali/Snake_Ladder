cc90815b7b7f65a32e5aa5850e4bbd381aaaed79
BashaVali <bashavali222@gmail.com>
2022-12-04 13:06:45 +0530

    Snake And Ladder Problem

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,33 @@
+﻿using SnakeAndLadder;
+using System;
+using System.Diagnostics;
+
+namespace LogicalProblems
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Snake And Ladder Programs");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("-------SnakeAndLadder Game ------");
+                Console.WriteLine("Select the option");
+                Console.WriteLine("1.GameStart" + "\n" +
+                                  "2.Exit" + "\n");
+                int options = Convert.ToInt32(Console.ReadLine());
+                switch (options)
+                {
+                    case 1:
+                        Starts starts = new Starts();
+                        starts.GameStart();
+                        break;
+                    case 12:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ SnakeAndLadder/Start.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class Starts
+    {
+        // Declare Local Variable
+        int PositionP1 = 0, PositionP2 = 0;int Dice=0;
+        public void GameStart()
+        {
+            Console.WriteLine("Game Start :" + PositionP1); 
+        }      
+    }
+}
